84af5efb1b24a7243a067e4f98099d6dabfb86bc
valen <haury@cip.ifi.lmu.de>
2026-08-09 19:11:36 +0200

    up-to-date

--- src/algorithm_components/fall_throughs/strict_tau_loop.py
+++ src/algorithm_components/fall_throughs/strict_tau_loop.py
@@ -1,10 +1,5 @@
-# wenn in einem Trace eine start-activity auf eine end-activity folgt
-# wird der Trace hier gesplittet, und der Log mit gesplitteten Traces wird
-# zurückgegeben und dann durch Loop mit einem empty-log connected
+# It's not possible to implement this with partial orders
 import copy
-
-from data_structures.trace import Trace
-
 
 def create_strict_tau_loop_log(traces, start_activities, end_activities):
     old_traces = copy.deepcopy(traces)
@@ -15,4 +10,4 @@ def create_strict_tau_loop_log(traces, start_activities, end_activities):
         changed = False
         for relation in old_trace.get_transitive_reduced_strict_partial_order():
             if relation.get_first().get_activity() in end_activities and relation.get_second.get_activity() in start_activities:
-                
+                print("Impossible")
