7b92e9b2c726bf90c4863353ca45a206d9eb8f6d
unknown <valentinhaury@gmail.com>
2026-08-28 09:13:10 +0200

    added comments

--- src/concurrency_miner_components/partitioning/parallel_partitioning.py
+++ src/concurrency_miner_components/partitioning/parallel_partitioning.py
@@ -13,6 +13,8 @@ def create_parallel_partitions(activities, overlapping_relations, eventually_fol
     partitions = [{activity} for activity in activities]
 
     # merge non-fully overlapping partitions
+    # -> if two activities are in a follows relation in any trace
+    # or if they are never overlapping (because they can still be exclusive)
     for a, b in combinations(activities, 2):
         if (a, b) in eventually_follows_relation or (b, a) in eventually_follows_relation:
             merge_partitions(a, b, partitions)
