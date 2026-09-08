2b68a87783b98218c48e2c2a1e43747855b79730
valen <haury@cip.ifi.lmu.de>
2026-09-08 08:31:33 +0200

    added filtering for start and end activitiies

--- src/infrequent_concurrency_miner.py
+++ src/infrequent_concurrency_miner.py
@@ -51,20 +51,22 @@ def infrequent_concurrency_miner(
                 log.append(trace)
 
 ##### initiate log parameters
-    log_activities, log_start_activities, log_end_activities, log_follows = set(), set(), set(), set()
-    counter_overlapping, counter_directly_follows = Counter(), Counter()
+    log_activities, log_follows = set(), set()
+    counter_overlapping, counter_directly_follows, counter_start_activities, counter_end_activities = Counter(), Counter(), Counter(), Counter()
 
     for trace in log:
         log_activities |= trace.get_activities()                        # contains all activities, activities are always represented by their name-string
-        log_start_activities |= trace.get_start_activities()            # contains all start activities
-        log_end_activities |= trace.get_end_activities()                # contains all end activities
+        #log_start_activities |= trace.get_start_activities()            # contains all start activities
+        counter_start_activities.update(trace.get_start_activities())
+        #log_end_activities |= trace.get_end_activities()                # contains all end activities
+        counter_end_activities.update(trace.get_end_activities())
         #log_overlapping_relation |= trace.get_overlapping_activities()  # contains pairs of activities that occur parallel at least once in the log
         counter_overlapping.update(trace.get_overlapping_activities())
         #log_directly_follows |= trace.get_directly_follows()            # contains pairs of activities where the second follows directly after the first in at least one trace
         counter_directly_follows.update(trace.get_directly_follows())
         log_follows |= trace.get_eventually_follows()                   # contains all pairs of activities where the second follows eventually after the first in at least one trace
 
-    def filter_relation(counter):
+    def filter_relation(counter, threshold):
         max_counts_out = {}
         max_counts_in = {}
         for (a, b), count in counter.items():
@@ -72,16 +74,29 @@ def infrequent_concurrency_miner(
             max_counts_in[b] = max(max_counts_in.get(b, 0), count)
         filtered_relation = set()
         for (a, b), count in counter.items():
-            if count / max_counts_out[a] >= filter_threshold or count / max_counts_in[b] >= filter_threshold:
+            if count / max_counts_out[a] >= threshold or count / max_counts_in[b] >= threshold:
                 filtered_relation.add((a, b))
         return filtered_relation
 
 ##### filter directly follows
-    filtered_directly_follows = filter_relation(counter_directly_follows)
+    filtered_directly_follows = filter_relation(counter_directly_follows, filter_threshold)
 
 ##### filter overlapping
-    filtered_overlapping = filter_relation(counter_overlapping)
+    filtered_overlapping = filter_relation(counter_overlapping, filter_threshold)
 
+    def filter_activities(counter, threshold):
+        max_count = 0
+        for _, count in counter.items():
+            max_count = max(max_count, count)
+        filtered_activities = set()
+        for a, count in counter.items():
+            if count / max_count >= threshold:
+                filtered_activities.add(a)
+        return filtered_activities
+
+##### filter start and end activities
+    filtered_start_activities = filter_activities(counter_start_activities, filter_threshold)
+    filtered_end_activities = filter_activities(counter_end_activities, filter_threshold)
 
     # create the transitive closure of the directly follows relation of the log
     log_eventually_follows = compute_transitive_closure(filtered_directly_follows)
@@ -110,13 +125,13 @@ def infrequent_concurrency_miner(
 
 ##### split the log with an interleaving operator
     print(f"[{datetime.now():%H:%M:%S}] Starting FILTERED Interleaving partitioning")
-    interleaving_partitions = create_interleaving_partitions(log_activities, log_start_activities, log_end_activities, filtered_overlapping, filtered_directly_follows, log_minimum_self_distance)
+    interleaving_partitions = create_interleaving_partitions(log_activities, filtered_start_activities, filtered_end_activities, filtered_overlapping, filtered_directly_follows, log_minimum_self_distance)
     if len(interleaving_partitions) > 1:
         return create_children_from_sublogs(Node(Operator.Interleaving), create_sublogs_general(log, interleaving_partitions), concurrency_miner, filter_threshold) #TODO implement NEW sublog creation for infrequent
 
 ##### split the log with a concurrent operator
     print(f"[{datetime.now():%H:%M:%S}] Starting FILTERED Concurrent partitioning")
-    concurrent_partitions = create_concurrent_partitions(log_activities, log_start_activities, log_end_activities, filtered_overlapping, filtered_directly_follows, log_minimum_self_distance)
+    concurrent_partitions = create_concurrent_partitions(log_activities, filtered_start_activities, filtered_end_activities, filtered_overlapping, filtered_directly_follows, log_minimum_self_distance)
     if len(concurrent_partitions) > 1:
         return create_children_from_sublogs(Node(Operator.Concurrent), create_sublogs_general(log, concurrent_partitions), concurrency_miner, filter_threshold) #TODO implement NEW sublog creation for infrequent
 
@@ -128,13 +143,13 @@ def infrequent_concurrency_miner(
 
 ##### split the log with a loop operator
     print(f"[{datetime.now():%H:%M:%S}] Starting FILTERED Loop partitioning")
-    loop_partitions = create_loop_partitions(log_activities, log_start_activities, log_end_activities, filtered_overlapping, filtered_directly_follows)
+    loop_partitions = create_loop_partitions(log_activities, filtered_start_activities, filtered_end_activities, filtered_overlapping, filtered_directly_follows)
     if len(loop_partitions) > 1:
         return create_children_from_sublogs(Node(Operator.Loop), create_sublogs_loop(log, loop_partitions), concurrency_miner, filter_threshold) #TODO implement NEW sublog creation for infrequent
 
 ##### split the log with an arbitrary order operator
     print(f"[{datetime.now():%H:%M:%S}] Starting FILTERED Arbitrary Order partitioning")
-    arbitrary_order_partitions = create_arbitrary_order_partitions(log, log_activities, log_start_activities, log_end_activities, filtered_overlapping, log_follows, filtered_directly_follows, log_minimum_self_distance)
+    arbitrary_order_partitions = create_arbitrary_order_partitions(log, log_activities, filtered_start_activities, filtered_end_activities, filtered_overlapping, log_follows, filtered_directly_follows, log_minimum_self_distance)
     if len(arbitrary_order_partitions) > 1:
         return create_children_from_sublogs(Node(Operator.Arbitrary), create_sublogs_general(log, arbitrary_order_partitions), concurrency_miner, filter_threshold) #TODO implement NEW sublog creation for infrequent
 
