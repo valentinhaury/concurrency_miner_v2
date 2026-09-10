6a6f3a205edb14c2c116084a57b270a9a24490a3
valen <haury@cip.ifi.lmu.de>
2026-09-10 06:02:46 +0200

    when creating traces made sure to make overlapping relation symmetrical

--- src/data_structures/trace.py
+++ src/data_structures/trace.py
@@ -7,6 +7,8 @@ class Trace:
         self.transitive_reduced_strict_partial_order = set(transitive_reduced_strict_partial_order)
         self.strict_partial_order = set(strict_partial_order)
         self.overlapping_relations = set(overlapping_relation)
+        for (e1, e2) in self.overlapping_relations:
+            self.overlapping_relations.add((e2, e1))
 
     def __repr__(self):
         if not self.events:
