4c314f2e051c7163f4af43dc0f1c10808ed43584
unknown <valentinhaury@gmail.com>
2026-09-17 08:00:34 +0200

    added more description to arbitrary order partitioning

--- src/concurrency_miner_components/partitioning/arbitrary_order_partitioning.py
+++ src/concurrency_miner_components/partitioning/arbitrary_order_partitioning.py
@@ -20,35 +20,25 @@ def create_arbitrary_order_partitions(traces, activities, start_activities, end_
         if (a, b) in minimum_self_distance_relations or (b, a) in minimum_self_distance_relations:
             merge_partitions(a, b, partitions)
 
-
-
     # merge all partitions that either have no start or no end activities to a partition they are always connected to
     partitions_to_merge = []
     for partition in partitions:
         if partition.isdisjoint(start_activities) or partition.isdisjoint(end_activities):
             partitions_to_merge.append(partition)
-    # TODO maybe do this following part multiple times until nothing changes anymore
     connect_partitions_relation = []
     for p1, p2 in product(partitions_to_merge, partitions):
         if p1 != p2:
-            always_direct_connected = True
-            for trace in traces:
-                trace_directly_follows = trace.get_directly_follows()
-                trace_direct_connected = False
-                for a1, a2 in product(p1, p2):
-                    if (a1, a2) in trace_directly_follows or (a2, a1) in trace_directly_follows:
-                        trace_direct_connected = True
-                        break
-                if not trace_direct_connected:
-                    always_direct_connected = False
-                    break
-            if always_direct_connected:
+            if _always_direct_connected(p1, p2, traces):
                 connect_partitions_relation.append((p1, p2))
+                partitions_to_merge.remove(p1)
     for relation in connect_partitions_relation:
         merge_partitions(next(iter(relation[0])), next(iter(relation[1])), partitions)
 
+    # TODO merge the rest with an arbitrary partition
+
     # merge all partitions that are not direct connected in both directions
     # TODO maybe merge them to partitions they are always connected to like with the start or end activities
+    # TODO merge the rest with an arbitrary partition
     if False:
         not_direct_connected = []
         for p1, p2 in combinations(partitions, 2):
@@ -77,3 +67,17 @@ def create_arbitrary_order_partitions(traces, activities, start_activities, end_
                     break
 
     return partitions
+
+def _always_direct_connected(p1, p2, traces):
+    always_direct_connected = True
+    for trace in traces:
+        trace_directly_follows = trace.get_directly_follows()
+        trace_direct_connected = False
+        for a1, a2 in product(p1, p2):
+            if (a1, a2) in trace_directly_follows or (a2, a1) in trace_directly_follows:
+                trace_direct_connected = True
+                break
+        if not trace_direct_connected:
+            always_direct_connected = False
+            break
+    return always_direct_connected
